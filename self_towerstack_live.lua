a9b70692fd914215a885d2e35bce203b8d48396c
herkle <69731592+decryp1@users.noreply.github.com>
2026-06-03 00:21:43 -0500

    added live tower stacker

---
+++ self_towerstack_live.lua
@@ -0,0 +1,4 @@
+_G.stackk = true; --[[set to false to disable (execute as a different script).]]--
+_G.offset = 5; --[[changes compatability, fixes some issues with some positions. once again, execute as a different script.]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/self_towerstack"))()
