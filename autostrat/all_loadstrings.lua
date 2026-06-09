57b876047427d697ceec441744bb936ac7b42151
herkle <69731592+decryp1@users.noreply.github.com>
2026-06-09 09:33:19 -0500

    nevermind i called it incorrectly

--- autostrat/all_loadstrings.lua
+++ autostrat/all_loadstrings.lua
@@ -1,4 +1,5 @@
 return {
+  local function piecestring(traffic) return loadstring(game:HttpGet(traffic))(); end
   ["autostrat"]       =  "https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/autostrat/autostrat.lua",
   ["gatlingexploit"]  =  "https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/autostrat/gatlingexploit.lua",
   ["joinhandler"]     =  "https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/autostrat/joinhandler.lua",
@@ -6,3 +7,8 @@ return {
   ["stratrecorder"]   =  "https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/autostrat/stratrecorder.lua",
   ["towerstacker"]    =  "https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/autostrat/towerstacker.lua"
 }
+--[[
+loadstrings = loadstring(game:HttpGet("https://raw.githubusercontent.com/decryp1/tower-defense-simulator/refs/heads/main/autostrat/all_loadstrings.lua"))()
+print(type(loadstrings.piecestring))
+print(loadstrings.piecestring(loadstrings["resources"]))
+]]
